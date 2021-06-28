38a99aa38e84e0a67b09bd7cb27f242ec5f90431
evangoldrick <84489887+evangoldrick@users.noreply.github.com>
2021-06-28 05:53:22 -0400

    Moved the simple gui to its own file, and changed the way errors are displayed

--- main.py
+++ main.py
@@ -1,39 +1,19 @@
 import requests
 import api_helper
 import gcode_parser
-import keyReader
+import key_reader
+import simple_gui
 
-import PySimpleGUI as sg
 import os
 import sys
 
+FORCE_SIMPLE = True
 
 def main(argv):
-    
-    if not ("--nogui" in argv or "-g" in argv):
-        print("init gui")
-
-        infoPanelLayout = [[sg.Text("NONE", key="TIME/LAYER")], [sg.Text("NONE", key="-FILE NAME-")], [sg.Button("QUIT", key="QUIT")]]
-        fullLayout = [[sg.Text("graph goes here", key="-IMAGE-")], [sg.ProgressBar(100, orientation="vertical", size=(600, 50), key="progress meter")], [sg.Column(infoPanelLayout)]]
-        window = sg.Window(title="Octoviewer Light", layout=fullLayout, size=(1280,720))
-
-        while True:
-
-            # add api stuff
-
-            event, values = window.read(timeout=1)
-
-            if event == "-QUIT-" or event == sg.WIN_CLOSED:
-                print("EVENT LOOP EXIT")
-                break
-            try:
-                window["progress meter"].update(float(api_helper.getResponse("http://octopi.local/api/job", keyReader.getKey()).json()["progress"]["completion"]))
-            except requests.exceptions.ConnectionError as e:
-                # Could not connect to api
-                window.Title = "No connection"
-                #window.
-        print("Close window")
-        window.close()
+    if "--nogui" in argv:
+        pass
+    elif "--simple" in argv or FORCE_SIMPLE:
+        simple_gui.start()
 
 
 if __name__ == "__main__":
