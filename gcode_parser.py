445fd511348875c102aeaff4ac8591da14f22542
evangoldrick <84489887+evangoldrick@users.noreply.github.com>
2021-07-16 13:01:39 -0400

    Added file reading, and byte counting

--- gcode_parser.py
+++ gcode_parser.py
@@ -1,8 +1,18 @@
 
-def parse(fileName):
+from array import array
+
+
+def readAndCountGCode(fileName) -> array:
     with open(fileName, "rb") as gCodeFile:
-        gCode = gCodeFile.read()
-        print(gCode)
+        gCode = gCodeFile.readlines()
+        byteCount = 0
+        gCodeWithCounts = list()
+        for line in gCode:
+            gCodeWithCounts.append([byteCount, str(line.strip())[2:-1]]) # Convert line to string and remove b'' from the lines, also add byte counts
+            byteCount = byteCount + len(line)
+        
+        return gCodeWithCounts
 
 if __name__ == "__main__":
-    parse()
+    print(readAndCountGCode("gCodeFiles/AI3M_70mm_SmallRod.gcode")[0][1])
+    
