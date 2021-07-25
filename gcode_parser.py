d2d087737444141ada4c16d38c8c53e3c72cd077
evangoldrick <84489887+evangoldrick@users.noreply.github.com>
2021-07-24 21:01:33 -0400

    Added proper way to convert from __bytes to str

--- gcode_parser.py
+++ gcode_parser.py
@@ -1,18 +1,16 @@
 
-from array import array
-
-
-def readAndCountGCode(fileName) -> array:
+def readAndCountGCode(fileName) -> list:
     with open(fileName, "rb") as gCodeFile:
         gCode = gCodeFile.readlines()
         byteCount = 0
         gCodeWithCounts = list()
         for line in gCode:
-            gCodeWithCounts.append([byteCount, str(line.strip())[2:-1]]) # Convert line to string and remove b'' from the lines, also add byte counts
+            gCodeWithCounts.append([byteCount, line.decode('UTF-8').strip()]) # Convert line to string and remove b'' from the lines, also add byte counts
             byteCount = byteCount + len(line)
         
         return gCodeWithCounts
 
+
 if __name__ == "__main__":
-    print(readAndCountGCode("gCodeFiles/AI3M_70mm_SmallRod.gcode")[0][1])
+    print(readAndCountGCode("gCodeFiles/AI3M_70mm_SmallRod.gcode")[0])
     
